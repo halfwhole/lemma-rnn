519dc9e4c55a5a8931db0fa17bdcc5ca7cde61ee
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2020-04-03 21:39:21 +0800

    Refactor rnn into separate file

--- test1/test1.py
+++ test1/test1.py
@@ -5,39 +5,16 @@ import sys
 import time
 import torch
 import torch.nn as nn
-from util import *
+
 from parse import *
+from rnn import RNN
+from util import *
 
 cuda = torch.device("cuda")
 
 usefulness, problemlemmas = get_usefulness_problemslemmas()
 all_letters = string.printable
 n_letters = len(all_letters)
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
-
-        self.hidden_size = hidden_size
-        # Input to hidden function
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        # Input to output function
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        # Softmax is only for categorical outputs. Ours is scalar output
-        # self.softmax = nn.LogSoftmax(dim=1)
-
-    # Called on each input
-    # Computes the outputs (and next hidden state)
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        # output = self.softmax(output)
-        return output, hidden
-
-    def initHidden(self):
-        # Could use torch.rand also
-        return torch.zeros(1, self.hidden_size).to(cuda)
 
 # Output is just a float, the proof length ratios
 output_size = 1
