1647f2f536e47c0cab82ca633751cc23b3b150c3
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2020-04-09 20:04:33 +0800

    Create lstm class

---
+++ test2/lstm.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+from cuda_check import device  
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTM, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size)
+        self.hidden2out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input):
+        lstm_out, _ = self.lstm(input)
+        output = self.hidden2out(lstm_out)
+        return output
